c589b1703f50249540c665cf228134c3a780804c
BT_1E10_03 <sample@bths.edu>
2022-03-22 13:45:50 -0400

    Initial commit

---
+++ src/Taxi.java
@@ -0,0 +1,26 @@
+public class Taxi extends Car {
+  private double fareCollected;
+
+  public Taxi(String licensePlate, double tollFee, int passengers, boolean isElectric, double fareCollected) {
+    super(licensePlate, tollFee, passengers, isElectric); // super refers to the Car constructor
+    this.fareCollected = fareCollected;
+  }
+
+  public double getFareCollected()
+  {
+    return fareCollected;
+  }
+
+  public boolean chargeAndDropOffRiders(double farePerRider)
+  {
+    fareCollected += farePerRider * (getPassengers() - 1);
+    return dropOffPassengers(getPassengers() - 1);
+  }
+
+  @Override
+  public void printInfo()
+  {
+    super.printInfo();
+    System.out.println("Total fare collected: " + fareCollected);
+  }
+}
